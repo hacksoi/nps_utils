9f227ca453bfa5877626ffbdbf85b1f8e83b16e0
hacksoi <sohackinp@gmail.com>
2017-10-21 22:31:44 -0700

    added things

--- nps_string.h
+++ nps_string.h
@@ -3,6 +3,8 @@
 
 #include <string.h>
 
+#include "nps_common_defs.h"
+
 /* Checks for equality excluding null-terminators. */
 internal inline bool32
 npsStringEqualsWeak(char *String1, char *String2)
@@ -15,9 +17,18 @@ npsStringEqualsWeak(char *String1, char *String2)
 internal inline bool32
 npsStringContains(char *String1, char *String2)
 {
-    char *String1End = String1 + strlen(String1);
-    size_t String2Length = strlen(String2);
+    if(!String1 || !String2)
+    {
+        return false;
+    }
 
+    size_t String2Length = strlen(String2);
+    if(String2Length == 0)
+    {
+        return false;
+    }
+
+    char *String1End = String1 + strlen(String1);
     for(; (size_t)(String1End - String1) >= String2Length; String1++)
     {
         if(npsStringEqualsWeak(String1, String2))
