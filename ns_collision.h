7b05b11907cca126a2e132760dda949628463c05
Nick Sohacki <nisohack@microsoft.com>
2019-05-24 16:42:57 -0700

    hello

--- ns_collision.h
+++ ns_collision.h
@@ -28,20 +28,65 @@ bool CheckCollidesWith(rect2 ABoundingBox, v2 ADelta,
     return false;
 }
 
-/* This function adjusts the entity deltas, which the caller applies after. */
+/* This function adjusts the entity deltas, which the caller applies after.
+   It returns the collisions in the specified array; each collision involves
+   2 entities, and therefore two collisions are generated for each collision -
+   one where entity X is EntityA is first, and one where entity Y is EntityB -
+   we do this because it is easier on the user. */
 void DoCollisionDetection(collision *Collisions, int CollisionsSize, int *NumCollisionsPtr,
                           rect2 *BoundingBoxes, v2 *InputDeltas, v2 *OutputDeltas, bool *AffectedByCollisions, bool *Skip,
                           int NumEntities)
 {
     *NumCollisionsPtr = 0;
 
-    float ClosestT = FLT_MAX;
-    axis ClosestAxis;
-    int EntityIdxCollidedWith;
     for_ (CurEntityIdx, NumEntities)
     {
-        if (AffectedByCollisions[CurEntityIdx])
+        float ClosestT = FLT_MAX;
+        axis ClosestAxis;
+        int EntityIdxCollidedWith;
+        for_ (EntityIdx, NumEntities)
         {
+            if (EntityIdx == CurEntityIdx)
+            {
+                continue;
+            }
+
+            if (!Skip[EntityIdx])
+            {
+                float t;
+                axis Axis;
+                bool CollidesWith = CheckCollidesWith(BoundingBoxes[CurEntityIdx], InputDeltas[CurEntityIdx],
+                                                      BoundingBoxes[EntityIdx], InputDeltas[EntityIdx],
+                                                      &t, &Axis);
+                if (CollidesWith)
+                {
+                    if (t < ClosestT)
+                    {
+                        ClosestT = t;
+                        ClosestAxis = Axis;
+                    }
+                }
+            }
+        }
+
+        bool CollisionOcurred = ClosestT != FLT_MAX;
+        if (CollisionOcurred)
+        {
+            /* Add the collision. */
+            ArrayPtrGetLastAndAddOne(collision *Collision, Collisions, CollisionsSize, *NumCollisionsPtr);
+            Collision->EntityA = CurEntityIdx;
+            Collision->EntityB = EntityIdxCollidedWith;
+
+            /* Adjust the delta of the axis that the collision happened on. */
+            OutputDeltas[CurEntityIdx][ClosestAxis] = ClosestT*InputDeltas[CurEntityIdx][ClosestAxis];
+
+            /* Get how much is left of the delta for the other axis. */
+            axis OtherAxis = (axis)(1 - ClosestAxis);
+            float InverseT = 1.0f - ClosestT;
+            v2 NewDelta = {};
+            NewDelta[OtherAxis] = InverseT*InputDeltas[CurEntityIdx][OtherAxis];
+
+            ClosestT = FLT_MAX;
             for_ (EntityIdx, NumEntities)
             {
                 if (EntityIdx == CurEntityIdx)
@@ -53,7 +98,7 @@ void DoCollisionDetection(collision *Collisions, int CollisionsSize, int *NumCol
                 {
                     float t;
                     axis Axis;
-                    bool CollidesWith = CheckCollidesWith(BoundingBoxes[CurEntityIdx], InputDeltas[CurEntityIdx],
+                    bool CollidesWith = CheckCollidesWith(BoundingBoxes[CurEntityIdx], NewDelta,
                                                           BoundingBoxes[EntityIdx], InputDeltas[EntityIdx],
                                                           &t, &Axis);
                     if (CollidesWith)
@@ -67,9 +112,11 @@ void DoCollisionDetection(collision *Collisions, int CollisionsSize, int *NumCol
                 }
             }
 
-            bool CollisionOcurred = ClosestT != FLT_MAX;
+            CollisionOcurred = ClosestT != FLT_MAX;
             if (CollisionOcurred)
             {
+                Assert(ClosestAxis == OtherAxis);
+
                 /* Add the collision. */
                 ArrayPtrGetLastAndAddOne(collision *Collision, Collisions, CollisionsSize, *NumCollisionsPtr);
                 Collision->EntityA = CurEntityIdx;
@@ -77,52 +124,6 @@ void DoCollisionDetection(collision *Collisions, int CollisionsSize, int *NumCol
 
                 /* Adjust the delta of the axis that the collision happened on. */
                 OutputDeltas[CurEntityIdx][ClosestAxis] = ClosestT*InputDeltas[CurEntityIdx][ClosestAxis];
-
-                /* Get how much is left of the delta for the other axis. */
-                axis OtherAxis = (axis)(1 - ClosestAxis);
-                float InverseT = 1.0f - ClosestT;
-                v2 NewDelta = {};
-                NewDelta[OtherAxis] = InverseT*InputDeltas[CurEntityIdx][OtherAxis];
-
-                ClosestT = FLT_MAX;
-                for_(EntityIdx, NumEntities)
-                {
-                    if (EntityIdx == CurEntityIdx)
-                    {
-                        continue;
-                    }
-
-                    if (!Skip[EntityIdx])
-                    {
-                        float t;
-                        axis Axis;
-                        bool CollidesWith = CheckCollidesWith(BoundingBoxes[CurEntityIdx], NewDelta,
-                                                              BoundingBoxes[EntityIdx], InputDeltas[EntityIdx],
-                                                              &t, &Axis);
-                        if (CollidesWith)
-                        {
-                            if (t < ClosestT)
-                            {
-                                ClosestT = t;
-                                ClosestAxis = Axis;
-                            }
-                        }
-                    }
-                }
-
-                CollisionOcurred = ClosestT != FLT_MAX;
-                if (CollisionOcurred)
-                {
-                    Assert(ClosestAxis == OtherAxis);
-
-                    /* Add the collision. */
-                    ArrayPtrGetLastAndAddOne(collision* Collision, Collisions, CollisionsSize, *NumCollisionsPtr);
-                    Collision->EntityA = CurEntityIdx;
-                    Collision->EntityB = EntityIdxCollidedWith;
-
-                    /* Adjust the delta of the axis that the collision happened on. */
-                    OutputDeltas[CurEntityIdx][ClosestAxis] = ClosestT*InputDeltas[CurEntityIdx][ClosestAxis];
-                }
             }
         }
     }
